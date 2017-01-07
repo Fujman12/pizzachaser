2e92608660822da9ca95596c5390561a4f5af279
Narek Martikyan <Narek Martikyan>
2017-01-08 01:39:57 +0200

    Project init

---
+++ app/main/views.py
@@ -0,0 +1,98 @@
+from flask import render_template, session, redirect, url_for, current_app,request
+from .. import db
+from ..models import City, Restaurant,State
+from ..email import send_email
+from . import main
+from .forms import NameForm
+from flask import jsonify
+import sys
+import json
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    states = State.query.all();
+
+    return render_template('index.html',states = states)
+
+@main.route('/country/<country_name>')
+def country(country_name):
+
+    if country_name.upper() == 'USA':
+        states = State.query.all();
+        return render_template('country_states.html',states = states)
+    else:
+        #current_country = Country.query.filter_by(name = country_name).first()
+        #cities_in_country =  current_country.cities.all()
+        return render_template('country_cities.html')
+
+@main.route('/state/<state>')
+def state(state):
+
+    #current_state = State.query.filter_by(state).first()
+    #current_state.cities.all()
+    try:
+        _state = State.query.filter_by(name = state).first()
+        cities = _state.cities.all()
+    except Exception as e:
+        pass
+
+    return render_template('state.html',state = state,cities = cities)
+
+@main.route('/city/<city_name>')
+def city(city_name):
+    #current_city = City.query.filter_by(name = current_city).first()
+    #rests_in_city = current_city.restaurants.all()
+
+    return render_template('city.html',city_name = city_name)
+
+@main.route('/pizza_restaurant/<restaurant_name>')
+def pizza_restaurant(restaurant_name):
+    current_restaurant= Restaurant.query.filter_by(name = restaurant_name).first()
+
+    return render_template('restaurant.html')
+
+@main.route('/map',methods=['GET', 'POST'])
+def map():
+
+	return render_template('map.html')
+
+@main.route('/result', methods=['GET','POST'])
+def result():
+
+    dictionary1 = {'a':1, 'b':2}
+    dictionary2 = {'asd':2, 'b':3,"narek":"krasava"}
+    s =[]
+    s.append(dictionary1.copy())
+    s.append(dictionary2.copy())
+    #print('Hello world!')
+    #print('Hello world!', file=sys.stderr)
+
+    return jsonify(result = s)
+
+
+@main.route('/rests_in_city', methods=['GET','POST'])
+def rests_in_city():
+    name = request.form['name']
+
+    #current_city = City.query.filter_by(name = name)[1]
+    #rests_in_city = current_city.restaurants.all()
+
+    #return jsonify(result = [e.serialize() for e in rests_in_city])
+    return None
+@main.route('/rests_in_country', methods=['GET','POST'])
+def rests_in_country():
+    name = request.form['name']
+
+    current_country = Country.query.filter_by(name = name).first()
+    rests_in_country = current_country.restaurants.all()
+
+    return jsonify(result = [e.serialize() for e in rests_in_country])
+
+@main.route('/rests_in_state', methods=['GET','POST'])
+def rests_in_state():
+    name = request.form['name']
+
+    current_state = State.query.filter_by(name = name).first()
+    rests_in_state = current_state.restaurants.all()
+
+    return jsonify(result = [e.serialize() for e in rests_in_country])
---
+++ app/models.py
@@ -0,0 +1,52 @@
+from . import db
+
+
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+    id = db.Column(db.Integer, primary_key=True, nullable = False)
+    name = db.Column(db.String(80), index = True, nullable = False)
+    city_id = db.Column(db.Integer, db.ForeignKey('cities.id'), nullable = False)
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable = False)
+    state_id = db.Column(db.Integer, db.ForeignKey('states.id'), nullable = True)
+    address = db.Column(db.String(150), nullable = False)
+    url = db.Column(db.String(520),nullable = True)
+
+    reviews = db.relationship('Review', backref='restaurant', lazy='dynamic')
+
+    def serialize(self):
+        return {
+            'name': self.name,
+            'address': self.address,
+            'url': self.url
+        }
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True, nullable = False)
+    text = db.Column(db.String(280), nullable = False)
+    user = db.Column(db.String(30), nullable = False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable = False)
+    username = db.Column(db.String(30),nullable = False)
+
+class City(db.Model):
+    __tablename__ = 'cities'
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    name = db.Column(db.String(25), nullable = False, index = True)
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable = False)
+    state_id = db.Column(db.Integer, db.ForeignKey('states.id'), nullable = True)
+    restaurants = db.relationship('Restaurant', backref='city', lazy='dynamic')
+
+class Country(db.Model):
+    __tablename__ = 'countries'
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    name = db.Column(db.String(20), nullable = False, index = True)
+    restaurants = db.relationship('Restaurant', backref='country', lazy='dynamic')
+    cities = db.relationship('City', backref='country', lazy='dynamic')
+
+class State(db.Model):
+    __tablename__ = 'states'
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    name = db.Column(db.String(25), nullable = False, index = True)
+    cities = db.relationship('City', backref='state', lazy='dynamic')
+    restaurants = db.relationship('Restaurant', backref='state', lazy='dynamic')
