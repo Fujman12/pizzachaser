aec8ae8aec788527304158c37f9a681216602660
Alexandr Zhuchinskiy <zhunya94@gmail.com>
2017-01-08 05:06:50 +0200

    hardcoded mysql conf

--- config.py
+++ config.py
@@ -32,8 +32,10 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql://pizzachaser:1q2w3e@localhost/pizzadb'
 
 
 config = {
